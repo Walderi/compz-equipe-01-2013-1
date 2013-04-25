01239ca298f915d21190ebb5b58edecfded6bafa
lsergiobp <lsergiobp@gmail.com>
2013-04-24 21:57:19 -0400

    Modificando o metodo de impressao de particoes

--- lista_particao/lista_particao.c
+++ lista_particao/lista_particao.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 
 //Método para imprimir cada partição p do conjunto de n elementos.
-void imprimeParticoes( int *p, int n ) {
+void imprimeParticoes( int *p, int n, int cont ) {
 	//Verifica o número de conjuntos na partição
     int numConjuntos = 1;
     int i;
@@ -17,22 +17,23 @@ void imprimeParticoes( int *p, int n ) {
 
     //Looping que imprime os conjuntos da partição
     int c;
+    printf( "%d - ", cont );
     for ( c = numConjuntos; c >= 1; --c ) {
-        printf("{");
+        printf("{ ");
         for ( i = 0; i < n; ++i ) {
         	//Verifica caso o p[i] for igual a c, então i+1 entrará nesse conjunto da particao
             if ( p[i] == c ) {
-                printf( "%d, ", i + 1 );
+                printf( "%d ", i + 1 );
             }
         }
-        printf("} ");
+        printf( "} ");
     }
     printf( "\n" );
 }
 
 //Método que valida se ainda existe alguma partição válida para imprimir.
 int existeParticaoValida( int *p, int *m, int n ) {
-//Ainda sem implementação.
+//Ainda sem implementação
 	return 0;
 }
 
