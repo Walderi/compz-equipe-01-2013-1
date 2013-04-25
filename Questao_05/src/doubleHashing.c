8e5c78268993f8cc40a812a5b02b2cc5377c5847
andrmore <ext-andre.moreira@nokia.com>
2013-04-25 11:25:43 -0400

    [QUESTAO05] DONE!!!

--- Questao_05/src/doubleHashing.c
+++ Questao_05/src/doubleHashing.c
@@ -9,12 +9,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-//#define matrixSize 100003
-#define matrixSize 17
-#define randomNumberRoofTop 100
-#define bufferSize 17
-//#define hashing2Constant 99991
-#define hashing2Constant 13
+#define matrixSize 100003
+//#define matrixSize 19
+#define randomNumberRoofTop 1000000
+//#define randomNumberRoofTop 100
+#define bufferSize 100000
+//#define bufferSize 17
+#define hashing2Constant 99991
+//#define hashing2Constant 17
 
 unsigned int hashMatrix[matrixSize];
 unsigned int randomNumbersBuffer[bufferSize];
@@ -32,7 +34,7 @@ void generateRandomNumbers()
 {
 	unsigned int i;
 	srand(time(NULL));
-	for(i=0; i<randomNumberRoofTop; i++)
+	for(i=0; i<bufferSize; i++)
 	{
 		randomNumbersBuffer[i] = rand() % randomNumberRoofTop;
 	}
@@ -61,7 +63,7 @@ void insertValue(unsigned int value)
 		while(hashMatrix[position] != NULL)
 		{
 			position =  (position + nextPosition);
-				if(position >= matrixSize )
+				if(position >= matrixSize)
 				{
 					position = (position - matrixSize);
 				}
@@ -70,29 +72,93 @@ void insertValue(unsigned int value)
 	}
 }
 
-unsigned int getValue()
+int findValue(unsigned int value)
 {
+	unsigned int position = hashing(value);
+	unsigned int nextPosition = hashing2(value);
+
+	if (hashMatrix[position] == NULL)
+	{
+		printf("value not found on hash table\n");
+		return -1;
+	}
+	else if (hashMatrix[position] == value)
+	{
+		printf("value found on hash table\n");
+		return (position);
+	}
+	else
+	{
+		while(hashMatrix[position] != NULL)
+		{
+			position =  (position + nextPosition);
+			if(position >= matrixSize )
+			{
+				position = (position - matrixSize);
+			}
+			if(hashMatrix[position] == value)
+			{
+				printf("value found on hash table\n");
+				return (position);
+			}
+		}
+		printf("value not found on hash table\n");
+		return -1;
+	}
 }
 
 void printHashMatrix()
 {
 	int i;
+	FILE * file;
+	file = fopen("matrix.txt","w+");
 	for(i = 0; i<matrixSize; i++)
 	{
-		printf("[%u] [%u] \n", i, hashMatrix[i]);
+	    if(hashMatrix[i] == NULL)
+        {
+        	fprintf(file, "[%u] [NULL]\n", i);
+        	//printf("[%u] [NULL] \n", i);
+        }
+        else
+        {
+        	fprintf(file, "[%u] [%u]\n", i, hashMatrix[i]);
+        	//printf("[%u] [%u] \n", i, hashMatrix[i]);
+        }
 	}
+	fclose(file);
 } 
+
+void insertAll()
+{
+	int i;
+	for(i = 0; i<bufferSize; i++)
+		{
+			insertValue(randomNumbersBuffer[i]);
+		}
+}
 
 void main()
 {
 	int i;
+	int value;
+	int position;
 	setHashMatrixNull();
 	generateRandomNumbers();
+    insertAll();
+    printHashMatrix();
+    printf("Enter the desired value or -1 to exit\n");
 
-	for(i = 0; i<bufferSize; i++)
-	{
-		insertValue(randomNumbersBuffer[i]);
-	}
-	printHashMatrix();
+    while( value!= -1 )
+    {
+    	scanf("%d", &value);
+        if(value != -1)
+        {
+        	position = findValue(value);
+        	if(position > -1)
+        	{
+        		printf("value stored on position %d \n", position);
+        	}
+        }
+    }
 }
 
