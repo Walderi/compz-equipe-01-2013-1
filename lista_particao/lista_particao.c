252e6cd8a69b7fa1afa665639284e149aa974657
lsergiobp <lsergiobp@gmail.com>
2013-04-24 22:17:15 -0400

    implementação do método de verificação de partição válida

--- lista_particao/lista_particao.c
+++ lista_particao/lista_particao.c
@@ -32,8 +32,40 @@ void imprimeParticoes( int *p, int n, int cont ) {
 }
 
 //Método que valida se ainda existe alguma partição válida para imprimir.
-int existeParticaoValida( int *p, int *m, int n ) {
-//Ainda sem implementação
-	return 0;
+int existeParticaoValida(int *p, int n) {
+	//Volta para o primeiro elemento para fazer as comparações
+	int i = 0;
+	++p[i];
+	//Seta o valor máximo pra 1;
+	p[n] = 1;
+
+	//Percorre o vetor a partir do segundo elemento para ver se tem algum valor maior que 1 para valor máximo
+	int j;
+	for (j = i + 1; j < n - 1; ++j) {
+		if ( p[j] > p[n] )
+			p[n] = p[j];
+	}
+
+	//Verifica se o 1o elemento é maior do que o valor de n, caso seja, significa que as partições acabaram e retorna false.
+	if ( p[0] == n + 1 )
+		return 0;
+
+	//Verifica se o p[i] é maior do que qualquer número após ele mais 1. Compara sempre com o valor máximo p[n].
+	while ((p[i] > p[n] + 1)) {
+		//Se for, seta o valor pra 1 e passa pro próximo elemento da lista e incrementa 1.
+		p[i] = 1;
+		++i;
+		++p[i];
+		p[n] = 1;
+		//Procura o valor máximo nos elementos após esse novo elemento para fazer uma nova comparação.
+		int j;
+		for (j = i + 1; j < n - 1; ++j) {
+			if ( p[j] > p[n] )
+				p[n] = p[j];
+		}
+	}
+
+	//Retorna um true dizendo que há mais partições.
+	return 1;
 }
 
