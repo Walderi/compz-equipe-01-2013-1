c2fcb4c4e6d652ffa9f6930d4d8f6707c5ec2c13
lsergiobp <lsergiobp@gmail.com>
2013-04-25 08:15:16 -0400

    Adicionado makefile para compilaçao do projeto

---
+++ Particoes/main.c
@@ -0,0 +1,29 @@
+#include "particoes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXTAM 11
+
+int main(int argc, char *argv[]) {
+	int p[MAXTAM];
+	int n = atoi( argv[1] );
+	int cont = 1;
+
+	//Seta todos os elementos do vetor como 1 para imprimir a partição de um só subconjunto
+	int i;
+	for (i = 0; i <= n; ++i) {
+		p[i] = 1;
+	}
+
+	imprimeParticoes( p, n, cont );
+	++cont;
+
+	//Procura nova partição válida e, caso tenha, imprime.
+	while ( existeParticaoValida( p, n ) ) {
+		imprimeParticoes( p, n, cont );
+		++cont;
+	}
+
+	//Fim do programa
+	return 0;
+}
+
