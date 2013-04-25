bced6082d7bbf7ef3aaad5c4089380de7c728d9c
andrmore <ext-andre.moreira@nokia.com>
2013-04-25 00:05:07 -0400

    [QUESTAO05] esqueleto do projeto

--- Questao_05/src/doubleHashing.c
+++ Questao_05/src/doubleHashing.c
@@ -1,7 +1,54 @@
+/*
+*  Double Hashing
+*
+*  by: André Moreira
+*
+*/
+
 #include <stdio.h>
+
+#define matrixSize 13
+
+unsigned int hashMatrix[matrixSize];
+
+void setHashMatrixNull()
+{
+}
+
+unsigned int hashing(int Key)
+{
+}
+
+unsigned int hashing2(int Key)
+{
+} 
+
+void insertValue()
+{
+}
+
+unsigned int getValue()
+{
+}
+
+void printValue()
+{
+} 
 
 void main() 
 {
-    printf("Hello World");	
+    printf("Hello World");
+
+//    int i = 0;
+//    for(i = 0; i<13; i++)
+//    {
+//        hashMatrix[i] = NULL;
+//    }
+
+//    if (hashMatrix[1] == NULL)
+//    {
+//    printf(" NULL ");
+//    }else{ printf(" Bug ");}	
+
 }
 
