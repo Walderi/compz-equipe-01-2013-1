ccadbabccb0557ec444d47b609bee923bacd7a21
gomesgeorgelucas <gomes.georgelucas@gmail.com>
2013-04-26 21:12:02 -0400

    Questão 2 - Main corrigido para pegar arquivo como parâmetro

--- lista_email/main.c
+++ lista_email/main.c
@@ -18,18 +18,39 @@
  * Questão 2 -
  */
 
-int main(void) {
-	//Variáveis de instância
-	char email[MAX_TAM_STR];
+int main(int argc, char *argv[]) {
+	if (argc != 2) { //argumentos incorretos
+		printf("Uso: %s nomedoarquivo\n", argv[0]); //mensagem de erro/ajuda
+	}
+	else {
 
-	while(scanf("%s",email) != EOF) {
-			if(verifica_email(email)) {
-				printf("%s %s\n", email, "Válido");
-			}
-			else {
-				printf("%s %s\n", email, "Inválido");
-			}
+		FILE *file;
+
+		file = fopen(argv[1],"r"); //tenta abrir arquivo
+
+		if (file == NULL) //se falha
+		{
+			printf( "Não conseguiu abrir arquivo\n" );
+			return(false);
 		}
+		else //caso consiga
+		{
+			//Variáveis de instância
+			char email[MAX_TAM_STR];
+
+			while(fscanf(file,"%s",email) != EOF) { //enquanto não for o fim do arquivo
+				if(verifica_email(email)) {
+					printf("%s %s\n", email, "Válido");
+				}
+				else {
+					printf("%s %s\n", email, "Inválido");
+				}
+			}
+
+			fclose(file); //fecha arquivo
+		}
+
+	}
 
 	return(true);
 }
